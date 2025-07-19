f715f6071f42b8f7d71eefaa38e11877d6103718
hiwonwon <hyewon9913@gmail.com>
2025-07-19 18:03:22 +0900

    signup login logout

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
+const sequelize = require("./config/database"); // Sequelize 인스턴스 import
+sequelize
+  .sync() // 테이블 자동 생성 or 동기화
+  .then(() => {
+    console.log("📦 Sequelize DB sync 완료");
+  })
+  .catch((err) => {
+    console.error("❌ DB sync 실패:", err);
+  });
 
 const mainRouter = require("./routes/mainRouter");
 const saveTestResultRouter = require("./routes/saveTestResultRouter");
 const etfFindRouter = require("./routes/etfFindRouter");
 const getTestResultRouter = require("./routes/getTestResult");
 const getTestThemeRouter = require("./routes/getTestThemeRouter");
+const userRouter = require("./routes/userRouter");
 const app = express();
 
 app.use(logger("dev"));
@@ -33,6 +43,7 @@ app.use("/api/me/mbti", saveTestResultRouter);
 app.use("/api/etfs", etfFindRouter);
 app.use("/api/recommendation", getTestResultRouter);
 app.use("/api/recommendation/theme", getTestThemeRouter);
+app.use("/api/auth", userRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
