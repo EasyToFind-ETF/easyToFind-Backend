9ddc97354dd8ab8f2bb23e846824448ba29c1c60
hiwonwon <hyewon9913@gmail.com>
2025-07-24 20:20:05 +0900

    modify test logic

--- dao/getTestResultDao.js
+++ dao/getTestResultDao.js
@@ -7,37 +7,45 @@ const getTestResultDao = {
     divScore
   ) => {
     const query = `
-     SELECT
-  ers.etf_code,
-  e.etf_name,
-  ers.stability_score,
-  ers.liquidity_score,
-  ers.growth_score,
-  ers.diversification_score,
-  ROUND((
+             WITH raw_score AS (
+  SELECT
+    ers.etf_code,
+    e.etf_name,
+    ers.stability_score,
+    ers.liquidity_score,
+    ers.growth_score,
+    ers.diversification_score,
+   ROUND((
+  (
     ers.stability_score * $1 +
     ers.liquidity_score * $2 +
     ers.growth_score * $3 +
     ers.diversification_score * $4
-  )::numeric, 2) AS total_score
-FROM etf_recommendation_score ers
-JOIN etfs e ON ers.etf_code = e.etf_code
-WHERE ers.stability_score IS NOT NULL
-  AND ers.liquidity_score IS NOT NULL
-  AND ers.growth_score IS NOT NULL
-  AND ers.diversification_score IS NOT NULL
+  ) / ($1 + $2 + $3 + $4)
+)::numeric, 2) AS total_score
+
+  FROM etf_recommendation_score ers
+  JOIN etfs e ON ers.etf_code = e.etf_code
+  WHERE ers.stability_score IS NOT NULL
+    AND ers.liquidity_score IS NOT NULL
+    AND ers.growth_score IS NOT NULL
+    AND ers.diversification_score IS NOT NULL
+)
+SELECT *
+FROM raw_score
 ORDER BY total_score DESC
 LIMIT 5;
 
 
 `;
     console.log(
-      "stabilityScore,liquidityScore,growthScore,divScore",
+      "testDao",
       stabilityScore,
       liquidityScore,
       growthScore,
       divScore
     );
+
     const result = await connection.query(query, [
       stabilityScore,
       liquidityScore,
